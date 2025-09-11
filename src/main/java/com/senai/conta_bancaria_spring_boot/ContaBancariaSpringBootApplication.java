399dc7f0696f8510d8dc0eb207b27f6c1b63334d
Amanda Marques <amandamarquescardozorosa@gmail.com>
2025-09-11 08:42:33 -0300

    Commit para teste de projeto

---
+++ src/main/java/com/senai/conta_bancaria_spring_boot/ContaBancariaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.senai.conta_bancaria_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaBancariaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaBancariaSpringBootApplication.class, args);
+	}
+
+}
