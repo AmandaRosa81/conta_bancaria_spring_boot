518d9ae6c00593a98e5d3b004540b0cf34a04611
Amanda Marques <amandamarquescardozorosa@gmail.com>
2025-11-25 22:05:42 -0300

    commit atualização - 25/11/2025

--- src/main/java/com/senai/conta_bancaria_spring_boot/Interface_ui/Controller/TaxaController.java
+++ src/main/java/com/senai/conta_bancaria_spring_boot/Interface_ui/Controller/TaxaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/taxas")
+@RequestMapping("/api/taxas")
 @RequiredArgsConstructor
 public class TaxaController {
 
